5bcd8c14f195b6321c85581ac94c551ad4799581
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-04-26 10:36:20 +0200

    initial commit

---
+++ src/main/java/com/muscledia/user_service/MusclediaUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.muscledia.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusclediaUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusclediaUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muscledia/user_service/MusclediaUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muscledia.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusclediaUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
